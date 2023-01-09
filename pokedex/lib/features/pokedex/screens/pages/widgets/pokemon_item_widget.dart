13ad54bd49b15215134592dc7e33d2cbb38ade45
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2023-01-09 11:19:28 -0300

    Continuando a tela dos pokemons

--- pokedex/lib/features/pokedex/screens/pages/widgets/pokemon_item_widget.dart
+++ pokedex/lib/features/pokedex/screens/pages/widgets/pokemon_item_widget.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:pokedex/common/models/pokemon.dart';
@@ -13,28 +13,52 @@ class PokemonItemWidget extends StatelessWidget {
   final Pokemon pokemon;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: pokemon.baseColor!.withOpacity(0.6), borderRadius: BorderRadius.circular(16)),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(pokemon.name),
-            Row(
-              children: [
-                Column(
-                  children: pokemon.type
-                      .map((e) => TypeWidget(
-                            name: e,
-                          ))
-                      .toList(),
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Container(
+        decoration: BoxDecoration(color: pokemon.baseColor!.withOpacity(0.8), borderRadius: BorderRadius.circular(16)),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRect(
+                      child: Text(
+                        pokemon.name,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          fontFamily: 'Google',
+                        ),
+                      ),
+                    ),
+                    Text(pokemon.num),
+                  ],
                 ),
-                Flexible(child: Image.network(pokemon.image))
-              ],
-            )
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: pokemon.type
+                        .map((e) => TypeWidget(
+                              name: e,
+                            ))
+                        .toList(),
+                  ),
+                  Flexible(child: Image.network(pokemon.image))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- pokedex/lib/features/pokedex/screens/pages/widgets/type_widget.dart
+++ pokedex/lib/features/pokedex/screens/pages/widgets/type_widget.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -11,8 +12,22 @@ class TypeWidget extends StatelessWidget {
   final String name;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(name),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 4),
+      child: Container(
+        decoration: BoxDecoration(color: Colors.white.withOpacity(0.2), borderRadius: BorderRadius.circular(16)),
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: Text(
+            name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
