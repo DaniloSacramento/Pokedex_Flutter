f3e78b2c65b84a44405cb7e9a3e39c6a40ea63ee
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2023-01-10 12:36:11 -0300

    Arrumando alguns erros

--- pokedex/lib/main.dart
+++ pokedex/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/common/repositorys/pokemon_repository.dart';
 import 'package:pokedex/features/home/container/home_container.dart';
@@ -8,7 +7,6 @@ import 'package:pokedex/features/pokedex/screens/login/pages/login/login_page.da
 
 void main() async {
   runApp(const MyApp());
-  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
@@ -18,13 +16,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Pokedex',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: const LoginPage(),
-      // PokedexRoute(
-      // repository: PokemonRepository(dio: Dio()),
-      //   ));
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Pokedex',
+        theme: ThemeData(primarySwatch: Colors.red),
+        home: //const LoginPage(),
+            PokedexRoute(
+          repository: PokemonRepository(dio: Dio()),
+        ));
+    //  );
   }
 }
